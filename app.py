0d3a4a002b2ee50402f70a3dbd7d2cc735e651e6
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-10-10 22:53:36 -0700

    Modify file

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import streamlit as st
 from operator import index
 import plotly.express as px
 from langchain.llms import OpenAI
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def generate_llm():
 
@@ -39,11 +41,34 @@ if __name__ == '__main__':
 
         if choice == "Sentiment":
             st.title("Sentiment Analysis")
+            wordcloud = WordCloud(background_color='white', width=800, height=800).generate(' '.join(df['Post content']))
+            st.image(wordcloud.to_image())
+
+
             generate_llm()
         if choice == "Time Series": 
             st.title("Exploratory Data Analysis")
+            df['Post Date'] = pd.to_datetime(df['Post Date'])
+            tweet_counts = df.groupby(df['Post Date'].dt.date).size().reset_index(name='Tweet Count')
+            time_series_plot = px.line(tweet_counts, x='Post Date', y='Tweet Count', title='Tweet Frequency Over Time', template='plotly_dark')
+            st.plotly_chart(time_series_plot)
             generate_llm()
-        if choice == "Modelling": 
+        if choice == "Geographical":
+            # Grouping data by Geo location and counting the number of tweets for each location
+            geo_counts = df['Geo location'].value_counts().reset_index()
+            geo_counts.columns = ['Geo location', 'Count']
+
+            # Using plotly to create a geographical scatter plot based on Geo location
+            geo_plot = px.scatter_geo(geo_counts, 
+                                    locations='Geo location',
+                                    locationmode='USA-states',
+                                    hover_name='Geo location',
+                                    size='Count',
+                                    title='Distribution of Tweets by Geo Location in USA',
+                                    template='plotly_dark',
+                                    scope='usa')
+            
+            st.plotly_chart(geo_plot)
             generate_llm()
         if choice == "Download": 
             generate_llm()
