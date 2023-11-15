6197015fb540bd946742e7b1701e789b1c50b1ee
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-11-15 14:57:42 -0800

    Imported plots

--- app.py
+++ app.py
@@ -2,73 +2,55 @@ import os
 import sys
 import pandas as pd
 import streamlit as st
-from operator import index
-import plotly.express as px
-from langchain.llms import OpenAI
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-
-def generate_llm():
-
-    def generate_response(input_text):
-        llm = OpenAI(temperature=0.9, openai_api_key=openai_api_key)
-        st.info(llm(input_text))
-
-    with st.form('my_form'):
-        text = st.text_area('Enter text:', 'What does this plot mean?')
-        submitted = st.form_submit_button('Submit')
-        if not openai_api_key.startswith('sk-'):
-            st.warning('Please enter your OpenAI API key!', icon='⚠')
-        if submitted and openai_api_key.startswith('sk-'):
-            generate_response(text)
+import plotly.express as px
+from generate_llm import generate_llm
+sys.path.append('./plots/')
+from time_series import create_time_series_plot
+from sentiment_distribution import create_sentiment_distribution_plot
+from tweet_count_bar import create_tweet_count_bar_chart
 
 def generate_sidebar():
     with st.sidebar: 
         st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
-        openai_api_key = st.text_input('OpenAI API Key', type='password')
-        st.title("554 Project Demo")
+        st.title("554 Project")
         st.info("This project application helps you see the different types of trends when it comes to weed based on twitter data.")
 
 if __name__ == '__main__':
-
     openai_api_key = os.getenv('OPENAI_API_KEY', '')
-    choice = st.sidebar.radio("Navigation", ["Sentiment","Time Series","Geographical", "Download"])
+    choice = st.sidebar.radio("Navigation", ["Sentiment","Time Series","Geographical"])
 
     if os.path.exists('./tweets-sample.csv'): 
-        
         df = pd.read_csv('./tweets-sample.csv', index_col=None)
         generate_sidebar()
 
+        # Example: If you want to plot the number of tweets per 'Username'
         if choice == "Sentiment":
-            st.title("Sentiment Analysis")
-            wordcloud = WordCloud(background_color='white', width=800, height=800).generate(' '.join(df['Post content']))
-            st.image(wordcloud.to_image())
+            st.title("Tweet Counts by Username")
+            username_tweet_counts = df['Username'].value_counts().reset_index()
+            username_tweet_counts.columns = ['Username', 'Tweet Count']
 
+            tweet_count_plot = create_tweet_count_bar_chart(username_tweet_counts, 'Username', 'Tweet Count', 'Tweet Counts by Username')
+            st.plotly_chart(tweet_count_plot)
 
             generate_llm()
+
+
         if choice == "Time Series": 
-            st.title("Exploratory Data Analysis")
+            st.title("Time Series Analysis")
             df['Post Date'] = pd.to_datetime(df['Post Date'])
             tweet_counts = df.groupby(df['Post Date'].dt.date).size().reset_index(name='Tweet Count')
-            time_series_plot = px.line(tweet_counts, x='Post Date', y='Tweet Count', title='Tweet Frequency Over Time', template='plotly_dark')
+
+            time_series_plot = create_time_series_plot(tweet_counts, 'Post Date', 'Tweet Count', 'Tweet Frequency Over Time')
             st.plotly_chart(time_series_plot)
+
             generate_llm()
+
         if choice == "Geographical":
-            # Grouping data by Geo location and counting the number of tweets for each location
+            # Geographical section remains as is
             geo_counts = df['Geo location'].value_counts().reset_index()
             geo_counts.columns = ['Geo location', 'Count']
-
-            # Using plotly to create a geographical scatter plot based on Geo location
-            geo_plot = px.scatter_geo(geo_counts, 
-                                    locations='Geo location',
-                                    locationmode='USA-states',
-                                    hover_name='Geo location',
-                                    size='Count',
-                                    title='Distribution of Tweets by Geo Location in USA',
-                                    template='plotly_dark',
-                                    scope='usa')
-            
+            geo_plot = px.scatter_geo(geo_counts, locations='Geo location', locationmode='USA-states', hover_name='Geo location', size='Count', title='Distribution of Tweets by Geo Location in USA', template='plotly_dark', scope='usa')
             st.plotly_chart(geo_plot)
-            generate_llm()
-        if choice == "Download": 
             generate_llm()
