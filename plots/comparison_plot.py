0b4433ff1244fe7a03350e6710dc83008c4c8470
Lorenzo Scaturchio <lorenzosca7@protonmail.ch>
2023-11-28 20:40:03 -0800

    Made comparison plot

--- plots/comparison_plot.py
+++ plots/comparison_plot.py
@@ -2,16 +2,10 @@ import pandas as pd
 import plotly.express as px
 
 def compare_medical_recreational(dispensaries):
-    """
-    Generates a bar chart comparing the number of medical and recreational dispensaries over time.
-
-    :param dispensaries: DataFrame containing dispensary data
-    :return: Plotly figure object
-    """
     # Filter and aggregate data
     grouped = dispensaries.groupby(['Year', 'Medical', 'Rec License']).size().unstack().fillna(0)
-    grouped['Total Medical'] = grouped[True].sum(axis=1)
-    grouped['Total Recreational'] = grouped[False].sum(axis=1)
+    grouped['Total Medical'] = grouped[1].sum(axis=1)
+    grouped['Total Recreational'] = grouped[0].sum(axis=1)
     grouped.reset_index(inplace=True)
 
     # Plotting
